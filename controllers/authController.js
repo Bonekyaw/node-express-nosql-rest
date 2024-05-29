bfc73a58dfeafe5c9cee3a12199a5f847d827186
bonekyaw <phonenai2014@gmail.com>
2024-05-29 12:08:09 +0630

    refresh token

--- controllers/authController.js
+++ controllers/authController.js
@@ -344,7 +344,7 @@ exports.refreshToken = [
     .trim()
     .notEmpty()
     .escape(),
-  body("user_id", "User ID must not be empty.").trim().notEmpty().escape(),
+  body("user_id", "User ID must not be empty.").isAlphanumeric(),
 
   asyncHandler(async (req, res, next) => {
     // Extract the validation errors from a request.
